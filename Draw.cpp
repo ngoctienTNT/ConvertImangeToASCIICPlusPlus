ce672e56602050c5f6e1a6faa692cd32caf5e816
Trần Ngọc Tiến <79617804+ngoctienTNT@users.noreply.github.com>
2021-08-10 08:29:32 +0700

    Add files via upload

---
+++ Draw.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+
+/**
+ *
+ * @author NgocTien.TNT
+ */
+
+using namespace std;
+
+void InputFile(string str)
+{
+	ifstream input(str);
+	char st[500];
+	while (input.getline(st, 500))
+	{
+		cout << st << endl;
+		Sleep(70);
+	}
+	input.close();
+}
+
+void OutputLoading()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Loading..." << i + 1 << "0%" << endl;
+		Sleep(500);
+	}
+    cout << "Successful loading...\n";
+}
+
+void OutputSpace()
+{
+    for (int i = 0; i < 15; i++)
+    {
+        cout << endl;
+        Sleep(70);
+    }
+}
+
+int main()
+{
+    char st[50];
+    bool kt = true;
+    do 
+    {
+        cout<<"Enter password: ";
+        cin.getline(st, 50);
+        if (strcmp(st,"tuthienka")==0)
+        {
+            cout<<"Password correct!\n";
+            Sleep(300);
+            OutputLoading();
+            kt = false;
+            InputFile("input1.inp");
+            OutputSpace();
+            InputFile("input2.inp");
+            OutputSpace();
+            InputFile("input3.inp");
+            OutputSpace();
+        }
+        else
+        {
+            cout<<"Sorry the password is incorrect! Please re-enter!\n";
+        }
+    } while (kt);
+}
